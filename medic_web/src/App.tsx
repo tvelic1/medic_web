c1513242c08986d398151d542a13e185bd6cfa68
tvelic1 <tvelic1@etf.unsa.ba>
2024-07-30 00:56:01 +0200

    initial

---
+++ medic_web/src/App.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import axios from 'axios';
+interface User {
+  id: number;
+  username: string;
+  password: string;
+  name: string;
+  prezime: string;
+  orders: number;
+  image_url: string;
+  date_of_birth: string;
+  last_login: string;
+  status: string;
+  role: string;
+}
+
+const API_URL = 'https://medic-api.vercel.app/s';
+
+export const fetchData = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await fetchData();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {data && (
+        <div className="data-display">
+          <h2>Fetched Data:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
