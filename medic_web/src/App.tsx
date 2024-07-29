876f96fdf88bc9a3c8165a923c2c7d07186f2e92
tvelic1 <tvelic1@etf.unsa.ba>
2024-07-30 01:06:32 +0200

    check vercel

--- medic_web/src/App.tsx
+++ medic_web/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          countt is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
